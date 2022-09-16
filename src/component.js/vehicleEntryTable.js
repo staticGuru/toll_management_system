35530bbd071f77baad97f7667c1a6e7174322955
staticGuru <vigneshguru274@gmail.com>
2022-09-16 23:15:55 +0530

    add the entry data

--- src/component.js/vehicleEntryTable.js
+++ src/component.js/vehicleEntryTable.js
@@ -1,33 +1,55 @@
-import React from 'react'
+import React from "react";
+import { VehicleEntryEnum } from "../enum";
 
 function VehicleEntryTable() {
-     const data = [
-          { name: "Anom", age: 19, gender: "Male" },
-          { name: "Megha", age: 19, gender: "Female" },
-          { name: "Subham", age: 25, gender: "Male"},
-        ]
+  const data = [
+    {
+      entry_id: "78",
+      vehicle_type: 1,
+      vehicle_number: "TN68MG1575",
+      entry_toll: { toll_id: "432", toll_name: "Madurai" },
+      tariff: 1235,
+      entry_time: "19/20/2022, 03:50:30",
+    },
+    {
+     entry_id: "72",
+     vehicle_type: 1,
+     vehicle_number: "TN68MG1575",
+     entry_toll: { toll_id: "432", toll_name: "Madurai" },
+     tariff: 1235,
+     entry_time: "19/20/2022, 03:50:30",
+   },
+  ];
   return (
-    <div style={{marginTop:'20px'}}>
-    <table style={{width: '100%'}}>
-    <tr>
-      <th style={{width:'40%',textAlign:'left',paddingLeft:'20px'}}>VEHICLE TYPE</th>
-      <th>VEHICLE NUMBER</th>
-      <th>DATE/TIME</th>
-      <th>TOLL NAME</th>
-      <th>TARIFF</th>
-    </tr>
-    {data.map((val, key) => {
-      return (
-        <tr key={key}>
-          <td style={{width:'40%',textAlign:'left',paddingLeft:'20px'}}>{val.name}</td>
-          <td>{val.age}</td>
-          <td>{val.gender}</td>
+    <div style={{ marginTop: "20px" }}>
+      <table style={{ width: "100%" }}>
+        <tr>
+          <th style={{ width: "40%", textAlign: "left", paddingLeft: "20px" }}>
+            VEHICLE TYPE
+          </th>
+          <th>VEHICLE NUMBER</th>
+          <th>DATE/TIME</th>
+          <th>TOLL NAME</th>
+          <th>TARIFF</th>
         </tr>
-      )
-    })}
-  </table>
+        {data.map((entry, key) => {
+          return (
+            <tr key={entry.entry_id}>
+              <td
+                style={{ width: "40%", textAlign: "left", paddingLeft: "20px" }}
+              >
+                {VehicleEntryEnum[entry.vehicle_type]}
+              </td>
+              <td>{entry.vehicle_number}</td>
+              <td>{entry.entry_time}</td>
+              <td>{entry.entry_toll.toll_name}</td>
+              <td>{entry.tariff}</td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
-  )
+  );
 }
 
-export default VehicleEntryTable
+export default VehicleEntryTable;
---
+++ src/enum.js
@@ -0,0 +1,6 @@
+export const VehicleEntryEnum={
+  1: "Car/Jeep/Van",
+  2:"LCV",
+  3:"Truck/Bus",
+  4:"Heavy Vehicle"
+}
