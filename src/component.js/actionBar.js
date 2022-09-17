842074c9308b3ab45223ea63020f989b6228c1d3
staticGuru <vigneshguru274@gmail.com>
2022-09-17 08:57:45 +0530

    add the form container

--- src/component.js/actionBar.js
+++ src/component.js/actionBar.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import filterIcon from "../assets/filter.png";
+import AddToll from "./addToll";
 import CustomButton from "./customButton";
 function ActionBar() {
+  useEffect(() => {
+    var modal = document.querySelector(".modal");
+    var trigger = document.querySelector(".trigger");
+    var closeButton = document.querySelector(".close-button");
+
+    function toggleModal() {
+      modal.classList.toggle("show-modal");
+    }
+
+    function windowOnClick(event) {
+      if (event.target === modal) {
+        toggleModal();
+      }
+    }
+    trigger?.addEventListener("click", toggleModal);
+    closeButton?.addEventListener("click", toggleModal);
+    window?.addEventListener("click", windowOnClick);
+    return()=>{
+      trigger?.removeEventListener("click", toggleModal);
+    closeButton?.removeEventListener("click", toggleModal);
+    window?.removeEventListener("click", windowOnClick);
+    }
+  });
      const showAddNewToll=()=>{
           console.log("called")
      }
@@ -36,7 +60,15 @@ function ActionBar() {
       <div style={{ display: "flex", flexDirection: "row" }}>
         <CustomButton onClick={() => {}}>add vehicle entry</CustomButton>
 
-        <CustomButton  className="trigger" id="myBtn" onClick={() => showAddNewToll()}>add new toll</CustomButton>
+        <CustomButton  className="trigger" onClick={() => showAddNewToll()}>add new toll</CustomButton>
+        <div>
+      <div className="modal">
+        <div className="modal-content">
+          <span className="close-button">×</span>
+          <div className="inner-content"><AddToll/></div>
+          </div>
+      </div>
+      </div>
         <CustomButton onClick={() => {}}>toll list</CustomButton>
       </div>
     </div>
--- src/component.js/customButton.js
+++ src/component.js/customButton.js
@@ -2,29 +2,7 @@ import React, { useEffect } from "react";
 import AddToll from "./addToll";
 
 function CustomButton(props) {
-  useEffect(() => {
-    var modal = document.querySelector(".modal");
-    var trigger = document.querySelector(".trigger");
-    var closeButton = document.querySelector(".close-button");
-
-    function toggleModal() {
-      modal.classList.toggle("show-modal");
-    }
-
-    function windowOnClick(event) {
-      if (event.target === modal) {
-        toggleModal();
-      }
-    }
-    trigger?.addEventListener("click", toggleModal);
-    closeButton?.addEventListener("click", toggleModal);
-    window?.addEventListener("click", windowOnClick);
-    return()=>{
-      trigger?.removeEventListener("click", toggleModal);
-    closeButton?.removeEventListener("click", toggleModal);
-    window?.removeEventListener("click", windowOnClick);
-    }
-  });
+  
   return (
     <div
       {...props}
@@ -38,14 +16,7 @@ function CustomButton(props) {
       }}
     >
       {props.children}
-      <div>
-      <div className="modal">
-        <div className="modal-content">
-          <span className="close-button">×</span>
-          <div className="inner-content">guru</div>
-          </div>
-      </div>
-      </div>
+      
     </div>
   );
 }
